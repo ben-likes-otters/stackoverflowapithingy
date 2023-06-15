9ff5c335d58e3eef7a044ba433345cae8da9578e
hyphen-slash <66840761+hyphen-slash@users.noreply.github.com>
2023-06-15 19:19:23 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from stackoverflowapithingy.stackoverflow import *
+from stackoverflowapithingy.stackoverflow import whatIsSettings, getQuestionByTags, getQuestionBySearch, getAnswerByQuestion, getAnswerById
