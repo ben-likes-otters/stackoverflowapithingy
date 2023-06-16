4e6e9e9d21390a47f2a0701b3f978e50587ab862
hyphen-slash <66840761+hyphen-slash@users.noreply.github.com>
2023-06-16 11:30:19 +0800

    Update stackoverflow.py

--- stackoverflowapithingy/stackoverflow.py
+++ stackoverflowapithingy/stackoverflow.py
@@ -18,6 +18,7 @@ def getQuestionBySearch(query,tags=["python"],q_per_page=1,page=1,accepted=True,
     url = "https://api.stackexchange.com/2.3/search/advanced?page={0}&pagesize={1}&order=desc&sort={2}&q={3}&accepted={4}&tagged={5}&site=stackoverflow&filter=withbody".format(str(page),str(q_per_page),["activity","votes","creation","relevance"][sort],query,accepted,formattedtags)
     return requests.get(url).json()
 
+
 def getAnswerByQuestion(question_id,sort=3):
     url = "https://api.stackexchange.com/2.3/questions/{0}/answers?order=desc&sort={1}&site=stackoverflow&filter=withbody".format(question_id,["activity","votes","creation","relevance"][sort])
     return requests.get(url).json()
@@ -26,6 +27,7 @@ def getAnswerByQuestion(question_id,sort=3):
 def getAnswerById(answer_id,sort=3):
     url = "https://api.stackexchange.com/2.3/answers/{0}?order=desc&sort={1}&site=stackoverflow&filter=withbody".format(answer_id,["activity","votes","creation","relevance"][sort])
     return requests.get(url).json()
+
 
 def formatResponse(response, isQuestion=True):  #False if it is an answer
     try:
@@ -44,7 +46,9 @@ def formatResponse(response, isQuestion=True):  #False if it is an answer
         return soup.get_text()
     except:
         raise ValueError("Invalid response passed")
-   
+        return
+
+    
 def getQandA(query,preformat=True,tags=["python"],q_per_page=1,page=1,accepted=True,sort=3):
     question = getQuestionBySearch(query,tags,q_per_page,page,accepted,sort)
     try:
@@ -58,3 +62,5 @@ def getQandA(query,preformat=True,tags=["python"],q_per_page=1,page=1,accepted=T
         return [formatResponse(question),formatResponse(answer,False)]
     else:
         return [question,answer]
+
+                                         
