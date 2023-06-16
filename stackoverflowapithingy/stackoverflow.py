4436194269d6aa1fafd21e02c0f083529bdae0b0
hyphen-slash <66840761+hyphen-slash@users.noreply.github.com>
2023-06-16 11:29:26 +0800

    Update stackoverflow.py
    Added a method to return both question and answer (faster)
    Added sorts for getAnswerById and getAnswerByQuestion

--- stackoverflowapithingy/stackoverflow.py
+++ stackoverflowapithingy/stackoverflow.py
@@ -7,24 +7,24 @@ def whatIsSettings():
     return "settings = {\"q_per_page\":1,\"accepted\":True,\"sort\":3,\"default_tags\":[\"python\"]}\noh and sort order is activity, votes, creation, relevance\nalso tags should be an array :)"
 
 
-def getQuestionByTags(tags=["python"],settings={"q_per_page":1,"accepted":True,"sort":3,"default_tags":["python"]}): #tags is array
+def getQuestionByTags(tags=["python"],q_per_page=1,page=1,accepted=True,sort=3): #tags is array
     formattedtags = ";".join(tags)
-    url = "https://api.stackexchange.com/2.2/questions?pagesize={0}&order=desc&sort=votes&tagged={1}&site=stackoverflow&filter=withbody".format(str(settings["q_per_page"]),formattedtags)
+    url = "https://api.stackexchange.com/2.2/questions?page={0}&pagesize={1}&order=desc&sort=votes&tagged={2}&site=stackoverflow&filter=withbody".format(str(page),str(q_per_page),formattedtags)
     return requests.get(url).json() #returns question object(s)
 
 
-def getQuestionBySearch(query,tags=["python"],settings={"q_per_page":1,"accepted":True,"sort":3,"default_tags":["python"]}):
+def getQuestionBySearch(query,tags=["python"],q_per_page=1,page=1,accepted=True,sort=3):
     formattedtags = ";".join(tags)
-    url = "https://api.stackexchange.com/2.3/search/advanced?pagesize={0}&order=desc&sort={1}&q={2}&accepted={3}&tagged={4}&site=stackoverflow&filter=withbody".format(settings["q_per_page"],["activity","votes","creation","relevance"][settings["sort"]],query,settings["accepted"],formattedtags)
+    url = "https://api.stackexchange.com/2.3/search/advanced?page={0}&pagesize={1}&order=desc&sort={2}&q={3}&accepted={4}&tagged={5}&site=stackoverflow&filter=withbody".format(str(page),str(q_per_page),["activity","votes","creation","relevance"][sort],query,accepted,formattedtags)
     return requests.get(url).json()
 
-def getAnswerByQuestion(question_id):
-    url = "https://api.stackexchange.com/2.3/questions/{0}/answers?order=desc&site=stackoverflow&filter=withbody".format(question_id)
+def getAnswerByQuestion(question_id,sort=3):
+    url = "https://api.stackexchange.com/2.3/questions/{0}/answers?order=desc&sort={1}&site=stackoverflow&filter=withbody".format(question_id,["activity","votes","creation","relevance"][sort])
     return requests.get(url).json()
 
 
-def getAnswerById(answer_id):
-    url = "https://api.stackexchange.com/2.3/answers/{0}?order=desc&site=stackoverflow&filter=withbody".format(answer_id)
+def getAnswerById(answer_id,sort=3):
+    url = "https://api.stackexchange.com/2.3/answers/{0}?order=desc&sort={1}&site=stackoverflow&filter=withbody".format(answer_id,["activity","votes","creation","relevance"][sort])
     return requests.get(url).json()
 
 def formatResponse(response, isQuestion=True):  #False if it is an answer
@@ -44,4 +44,17 @@ def formatResponse(response, isQuestion=True):  #False if it is an answer
         return soup.get_text()
     except:
         raise ValueError("Invalid response passed")
+   
+def getQandA(query,preformat=True,tags=["python"],q_per_page=1,page=1,accepted=True,sort=3):
+    question = getQuestionBySearch(query,tags,q_per_page,page,accepted,sort)
+    try:
+        answer = getAnswerById(question['items'][0]['accepted_answer_id'])
+    except:
+        try:
+            answer = getAnswerByQuestion(question['items'][0]['question_id']
+        raise ValueError("No accepted answer")
     
+    if preformat:
+        return [formatResponse(question),formatResponse(answer,False)]
+    else:
+        return [question,answer]
