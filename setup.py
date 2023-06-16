78df4c8c27433c1ec9e57a2c16e1945b018f1c2b
hyphen-slash <66840761+hyphen-slash@users.noreply.github.com>
2023-06-16 10:55:15 +0800

    Update setup.py
    we need bs4 now

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     name="stackoverflowapithingy",
     version="0.1.7",
     packages=["stackoverflowapithingy"],
-    install_requires=["requests"],
+    install_requires=["requests","beautifulsoup4"],
 )
