627718c3bbf6b34917a764fe0d503e218e32fa3b
hyphen-slash <66840761+hyphen-slash@users.noreply.github.com>
2023-06-16 11:35:30 +0800

    Update setup.py
    incrementing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="stackoverflowapithingy",
-    version="0.1.7",
+    version="0.1.12",
     packages=["stackoverflowapithingy"],
     install_requires=["requests","beautifulsoup4"],
 )
